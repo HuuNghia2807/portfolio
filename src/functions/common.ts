c6693df657d734444a3f33b11b9ea8e6b4078faf
HuuNghia2807 <100059003+HuuNghia2807@users.noreply.github.com>
2025-03-23 13:39:54 +0000

    Deploying to gh-pages from @ HuuNghia2807/portfolio@da15645fc7a7d36000f5a24197582fab866d882a 🚀

--- src/functions/common.ts
+++
@@ -1,6 +0,0 @@
-import ScrollReveal from "scrollreveal";
-const sr = ScrollReveal();
-
-export const animateFunc = (list: string, position: string) => {
-  sr.reveal(list, { origin: position });
-};
